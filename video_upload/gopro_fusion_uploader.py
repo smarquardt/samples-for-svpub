04478eba8c11ef031698a32b0802a11461c55ee8
smarquardt <36714019+smarquardt@users.noreply.github.com>
2018-07-30 20:07:16 -0700

    Fixing upload_video

--- video_upload/gopro_fusion_uploader.py
+++ video_upload/gopro_fusion_uploader.py
@@ -167,26 +167,28 @@ def request_upload_url():
 
 
 def upload_video(video_file, upload_url):
-  """Uploads video file to Street View via Upload URL.
+  """Uploads a the video bytes to SV servers (step 2/3).
 
   Args:
-    video_file: The video file to upload.
-    upload_url: The upload URL, provided by SV Publish API in step 1.
-
+    photo_file: Full path of the photo to upload.
+    upload_url: The upload URL returned by step 1.
   Returns:
     None.
   """
+  parsed_url = urlparse.urlparse(upload_url)
+  host = parsed_url[1]
   credentials = get_credentials()
-  http = credentials.authorize(httplib2.Http())
+  credentials.authorize(httplib2.Http())
   file_size = get_file_size(str(video_file))
   try:
-      headers = {"Authorization": "Bearer " + credentials.access_token,
+      h = {"Authorization": "Bearer " + credentials.access_token,
                  "Content-Type": "video/mp4",
                   "X-Goog-Upload-Protocol": "raw",
-                  "X-Goog-Upload-Content-Length": str(file_size)
+                  "X-Goog-Upload-Content-Length": str(file_size),
+                  "Host": host
                  }
-      files = {'file': ('upload_file', open(video_file, 'rb'))}
-      requests.post(upload_url, headers=headers, files=files)
+      with open(video_file,"rb") as upload_target:
+          r = requests.post(upload_url,headers=h,data=upload_target)
   except Exception as error_message:
       print(error_message)
 
